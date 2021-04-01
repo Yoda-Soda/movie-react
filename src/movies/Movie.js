478b40833ecbb0ee70d2a5cd776754cc880787a0
Yoda-Soda <janclaasen@gmail.com>
2021-04-01 15:25:09 +1300

    added images and css

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -1,17 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, config }) => {
   return (
-    <div>
-      <li>{movie.title}</li>
-    </div>
+    <li>
+      {/* insures that if config hasn't loaded before image url that it gets ignored */}
+      {config.images?.base_url && (
+        <img
+          src={config.images.base_url + "w342" + movie.poster_path}
+          alt={`${movie.title} Poster`}
+        />
+      )}
+      <h3>{movie.title}</h3>
+    </li>
   );
 };
 
 Movie.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -6,33 +6,44 @@ const POPULAR_API_URL =
   "https://api.themoviedb.org/3/discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&api_key=" +
   process.env.REACT_APP_MOVIE_API;
 
+const CONFIG_URL = `https://api.themoviedb.org/3/configuration?api_key=${process.env.REACT_APP_MOVIE_API}`;
+
 const MoviesList = () => {
   const [filter, setFilter] = useState("");
   const [movies, setMovies] = useState([]);
+  const [config, setConfig] = useState({});
 
   const getMovies = async () => {
     try {
       const res = await fetch(POPULAR_API_URL);
       const movies = await res.json();
       setMovies(movies.results);
-      //console.log("movies", movies);
+    } catch (error) {}
+  };
+
+  const getConfig = async () => {
+    try {
+      const res = await fetch(CONFIG_URL);
+      const config = await res.json();
+      setConfig(config);
     } catch (error) {}
   };
 
   useEffect(() => {
+    getConfig();
     getMovies();
   }, []);
 
   return (
     <div>
       <Filter filter={filter} setFilter={setFilter} />
-      <ul>
+      <ul className="movies-list">
         {movies
           .filter((movie) => {
             return movie.title.toLowerCase().includes(filter.toLowerCase());
           })
           .map((movie) => (
-            <Movie key={movie.id} movie={movie} />
+            <Movie key={movie.id} config={config} movie={movie} />
           ))}
       </ul>
     </div>
